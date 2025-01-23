c186616c3c8c7a23159aaf2bb93b2bc441b513a5
SabinaGarayeva <sgarayeva13@gmail.com>
2025-01-23 22:40:13 +0400

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
                 <img src="/snake-1.png" className="w-full" alt="" />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
-                Your Image on a
-                <span className="bg-green-600 px-2 text-white">Custom</span>
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
                 Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
